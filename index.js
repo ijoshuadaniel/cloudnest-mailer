0d2ff847d36b5d9f084e6ccb58636b9877ce40bb
Josh <josh@Joshs-Laptop.local>
2024-10-19 23:07:22 +0530

    fixes

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function processXlsxFile(inputXlsxPath, outputJsonPath) {
   saveEmailsToJson(emailIDs, outputJsonPath);
 }
 
-const inputFilePath = path.join(__dirname, "compares.xlsx");
-const outputFilePath = path.join(__dirname, "compare.json");
+const inputFilePath = path.join(__dirname, "input.xlsx");
+const outputFilePath = path.join(__dirname, "emails.json");
 
 processXlsxFile(inputFilePath, outputFilePath);
